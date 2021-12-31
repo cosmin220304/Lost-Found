54f8f428314ffe6703f8956ce5d05433de3a5051
Cosmin <cosmin.aftanase@yahoo.com>
2021-12-31 13:04:59 +0200

    add post screen

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,3 +1,4 @@
+import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
 
 function Home() {
--- src/components/screens/Post.js
+++ src/components/screens/Post.js
@@ -1,7 +1,31 @@
+import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
 
 function Post() {
-  return <div>Post screen</div>;
+  return (
+    <Flex
+      direction={['column']}
+      textAlign="center"
+      backgroundColor={'twitter.50'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      h="100%"
+    >
+      <Button
+        bg={'twitter.900'}
+        h="16"
+        w="90%"
+        color="white"
+        variant="outline"
+        mb="12"
+      >
+        I lost an object
+      </Button>
+      <Button bg={'twitter.900'} h="16" w="90%" color="white" variant="outline">
+        I found an object
+      </Button>
+    </Flex>
+  );
 }
 
 export default Post;
